c2a00862c6509e58983e7879b0435bfd72bb32bf
Kemal GENİŞ <kemal.genis@turkticaret.net>
2015-06-12 14:22:17 +0300

    analytics entegrasyonu

--- index.php
+++ index.php
@@ -38,6 +38,15 @@ include_once "init.php";
 	<p><b>Ajans Haber:</b> http://www.ajanshaber.com/bmwden-mercedese-gozdagi-video/199359</p>
 	<p><b>Web.tv:</b> http://onuradiguzel.web.tv/video/hakan-durmazlar-feat-bbc-msne-gel__zwxwbjsiyqc</p>
 </div>
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-20366275-34', 'auto');
+	ga('send', 'pageview');
+</script>
 </body>
 </html>
 <?php
